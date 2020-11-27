0fa57ef96a1f303e720b7ec8d0185776688c6bba
Giulio Graziano <giulio.graziano@eng.it>
2020-11-27 11:21:08 +0100

    added handlebars helpers: data and url extracting

--- src/helpers/hbs.js
+++ src/helpers/hbs.js
@@ -1,6 +1,11 @@
 const Handlebars = require('handlebars')
 
 
+// https://stackoverflow.com/a/37511463
+const normalizeWord = word => word.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+const getVarArgs = (args) => [...args].splice(0, args.length - 1)
+const getRootData = (args) => args[args.length - 1].data._parent.root
+
 Handlebars.registerHelper('object', function({ hash }) {
   //console.log("object-----------------")
   //console.log(hash)
@@ -9,7 +14,7 @@ Handlebars.registerHelper('object', function({ hash }) {
 Handlebars.registerHelper('array', function() {
   //console.log("array-----------------")
   //console.log(arguments)
-  return Array.from(arguments).slice(0, arguments.length-1)
+  return getVarArgs(arguments); // Array.from(arguments).slice(0, arguments.length-1)
 })
 /*Handlebars.registerHelper("metadata", function(data) {
   //console.log("metadata---------------------------")
@@ -25,7 +30,7 @@ Handlebars.registerHelper("toArray", function(data) {
 });
 
 Handlebars.registerHelper('route-isCurrent', function(activeClasses, relUrl) {
-  const { route: { location }, host: { baseUrl } } = this;
+  const { route: { location }, host: { baseUrl } } = getRootData(arguments); // this;
 
   const url = new URL(relUrl, baseUrl);
   const isCurrent = url.href == location.href;
@@ -36,7 +41,7 @@ Handlebars.registerHelper('route-isCurrent', function(activeClasses, relUrl) {
   return isCurrent;
 });
 Handlebars.registerHelper('route-isRelative', function(activeClasses, ...relUrls) {
-  const { route: { location }, host: { baseUrl } } = this;
+  const { route: { location }, host: { baseUrl } } = getRootData(arguments); // this;
 
   return relUrls
   .splice(0, relUrls.length - 1)
@@ -46,8 +51,8 @@ Handlebars.registerHelper('route-isRelative', function(activeClasses, ...relUrls
   }) ? activeClasses : '';
 });
 Handlebars.registerHelper('route-a', function() {
-  const { route: { location }, host: { baseUrl } } = this;
-  const [ relUrl, title, activeClasses = '', additionalClasses = '', attrs = '' ] = [...arguments].splice(0, arguments.length - 1)
+  const { route: { location }, host: { baseUrl } } = getRootData(arguments); // this;
+  const [ relUrl, title, activeClasses = '', additionalClasses = '', attrs = '' ] = getVarArgs(arguments); // [...arguments].splice(0, arguments.length - 1)
 
   const url = new URL(relUrl, baseUrl);
   const isCurrent = url.href == location.href;
@@ -59,9 +64,64 @@ Handlebars.registerHelper('route-a', function() {
   `);
 });
 
-Handlebars.registerHelper("routes-each", function(data, sortBy) {
-  return Object.entries(data)
+Handlebars.registerHelper("routes-each", function(routesSet, sortBy) {
+  return Object.entries(routesSet)
   .map(([_, value]) => value)
   .filter(({ isContent }) => isContent)
   .sort((a, b) => a[sortBy] > b[sortBy] ? 1 : -1);
 });
+
+Handlebars.registerHelper('urlify-route', function() {
+  const { host: { baseUrl } } = getRootData(arguments)
+  const urlParts = getVarArgs(arguments)
+  const url = parse(urlParts.map(normalizeWord).join('/')).href + '.html';
+  return new URL(url, baseUrl);
+})
+
+/*Handlebars.registerHelper('urlify-resx', function() {
+  const { host: { resxUrl } } = getRootData(arguments);
+  const urlParts = getVarArgs(arguments)
+  const url = parse(urlParts.map(normalizeWord).join('/')).href;
+  return new URL(url, resxUrl);
+})*/
+
+const extract = ([ data, property, compareProperty = null], isMany = false) => {
+  let result = Array.isArray(data) ? data : Object.entries(data)
+  .map(([_, value]) => value);
+
+  result = result
+  .map(item => get(item, property));
+
+  if (isMany) {
+    result = result
+    .reduce((resList, valueList) => [ // many
+      ...resList,
+      ...valueList
+    ], []);
+  }
+
+  if (compareProperty) {
+    result = result
+    .reduce((_list, value) => [
+      ..._list,
+      (_list.some(v => get(v, compareProperty) === get(value, compareProperty)) ? null : value)
+    ], [])
+  } else {
+    result = result
+    .reduce((_list, value) => [
+      ..._list,
+      (_list.some(v => v === value) ? null : value)
+    ], [])
+  }
+
+  return result
+  .filter(Boolean);
+}
+
+Handlebars.registerHelper('extract', function() {
+  return extract(getVarArgs(arguments));
+})
+
+Handlebars.registerHelper('extract-many', function() {
+  return extract(getVarArgs(arguments), true);
+})
