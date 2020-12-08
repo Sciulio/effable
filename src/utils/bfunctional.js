f701553a12e4c16de16258306a0d3de2550fb691
Sciulio <sciulio@msn.com>
2020-12-08 18:18:34 +0100

    added carry functional

--- src/utils/bfunctional.js
+++ src/utils/bfunctional.js
@@ -3,5 +3,7 @@ module.exports = {
   or: (...funcs) => arg => funcs.some(func => func(arg)),
   not: (func) => (...args) => !func(...args),
 
+  carry: (func, argsToSlice = 1) => (...args) => func(...args.slice(argsToSlice)),
+
   filterByExt: (...extList) => ({ ext }) => extList.includes(ext)
 }
